03682cd092e1e88da06ec25bdced1510ad003466
Xzilleon2 <Xzilleon2@gmail.com>
2026-02-22 14:05:58 +0800

    template for db

--- Assets/appointment.sql
+++ Assets/appointment.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 22, 2026 at 06:37 AM
+-- Generation Time: Feb 22, 2026 at 07:05 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -48,6 +48,27 @@ INSERT INTO `appointment` (`Appointment_ID`, `FULL_NAME`, `BIRTHDAY`, `AGE`, `GE
 (1, 'Jin Normham', '2005-10-01', 20, 'Male', '7778777898', 'Yame@gmail.com', 'Te', '13:02:00', '2026-02-22'),
 (2, 'Jin Normham', '2000-11-10', 25, 'Male', '8888999899', 'Yamite@gmail.com', 'Kudasai', '13:21:00', '2026-02-22');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `USER_ID` int(11) NOT NULL,
+  `EMAIL` varchar(255) NOT NULL,
+  `NAME` varchar(255) NOT NULL,
+  `PASSWORD` varchar(255) NOT NULL,
+  `DATE_CREATED` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`USER_ID`, `EMAIL`, `NAME`, `PASSWORD`, `DATE_CREATED`) VALUES
+(2, 'Zigaral00@gmail.com', 'Joseph', '$2y$10$uQYU08E1XSF6pVbDca9lReYMWQtzNAnCB4156E2bwg7cIKOWNb.oe', '2026-02-22');
+
 --
 -- Indexes for dumped tables
 --
@@ -59,6 +80,12 @@ ALTER TABLE `appointment`
   ADD PRIMARY KEY (`Appointment_ID`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`USER_ID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -67,6 +94,12 @@ ALTER TABLE `appointment`
 --
 ALTER TABLE `appointment`
   MODIFY `Appointment_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `USER_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
